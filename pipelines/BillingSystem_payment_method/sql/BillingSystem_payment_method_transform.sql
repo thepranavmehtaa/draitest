e97aea7d52b06425b1b308338b9f8fd98db1af9c
thepranavmehtaa <thepranavmehtaa@gmail.com>
2025-07-22 15:48:35 +0530

    Add data pipeline artifacts for BillingSystem_payment_method

---
+++ pipelines/BillingSystem_payment_method/sql/BillingSystem_payment_method_transform.sql
@@ -0,0 +1,21 @@
+-- SQL Code for Entity: BillingSystem_payment_method
+-- Create curated table with transformations
+
+CREATE TABLE IF NOT EXISTS BillingSystem_payment_method (
+    id BIGINT,
+    name VARCHAR(255),
+    description VARCHAR(255),
+    created_at VARCHAR(255),
+    updated_at TIMESTAMP
+);
+
+-- Insert transformed data
+INSERT INTO BillingSystem_payment_method
+SELECT
+    id as id,
+    name as name,
+    description as description,
+    created_at as created_at,
+    updated_at as updated_at
+FROM raw_BillingSystem_payment_method;
+
