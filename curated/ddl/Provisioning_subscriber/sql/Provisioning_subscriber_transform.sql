037d7773a76ea0629294d428deb9217f0bbe5c42
thepranavmehtaa <thepranavmehtaa@gmail.com>
2025-08-26 14:31:56 +0530

    Add data pipeline artifacts for Provisioning_subscriber in curated/ddl, curated/nlp, and curated/dml folders

--- curated/ddl/Provisioning_subscriber/sql/Provisioning_subscriber_transform.sql
+++ curated/ddl/Provisioning_subscriber/sql/Provisioning_subscriber_transform.sql
@@ -2,29 +2,10 @@
 -- Create curated table with transformations
 
 CREATE TABLE IF NOT EXISTS Provisioning_subscriber (
-    sub_id BIGINT,
-    contract_ref VARCHAR(255),
-    msisdn VARCHAR(255),
-    sim VARCHAR(255),
-    device_ref VARCHAR(255),
-    sub_type VARCHAR(255),
-    sub_status VARCHAR(255),
-    activated VARCHAR(255),
-    updated_at TIMESTAMP,
-    version VARCHAR(255),
-    is_deleted BOOLEAN
 );
 
 -- Insert transformed data
 INSERT INTO Provisioning_subscriber
 SELECT
-    sub_id as sub_id,
-    contract_ref as contract_ref,
-    msisdn as msisdn,
-    sim as sim,
-    device_ref as device_ref,
-    sub_type as sub_type,
-    sub_status as sub_status,
-    activated as activated
 FROM raw_Provisioning_subscriber;
 
