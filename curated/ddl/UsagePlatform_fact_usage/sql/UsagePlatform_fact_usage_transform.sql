5694b189ef9ba971bca2d0f3bec4d74d1d6c6e9b
thepranavmehtaa <thepranavmehtaa@gmail.com>
2025-08-26 18:09:26 +0530

    Update UsagePlatform_fact_usage_transform.sql

--- curated/ddl/UsagePlatform_fact_usage/sql/UsagePlatform_fact_usage_transform.sql
+++ curated/ddl/UsagePlatform_fact_usage/sql/UsagePlatform_fact_usage_transform.sql
@@ -8,10 +8,6 @@ CREATE TABLE IF NOT EXISTS UsagePlatform_fact_usage (
     type VARCHAR(255),
     volume VARCHAR(255),
     minutes VARCHAR(255),
-    sms VARCHAR(255),
-    region VARCHAR(255),
-    roaming VARCHAR(255),
-    updated_at TIMESTAMP,
     version VARCHAR(255),
     is_deleted BOOLEAN
 );
@@ -20,13 +16,5 @@ CREATE TABLE IF NOT EXISTS UsagePlatform_fact_usage (
 INSERT INTO UsagePlatform_fact_usage
 SELECT
     usage_id as usage_id,
-    sub_id as sub_id,
-    usage_date as usage_date,
-    type as type,
-    volume as volume,
-    minutes as minutes,
-    sms as sms,
-    region as region,
-    roaming as roaming
 FROM raw_UsagePlatform_fact_usage;
 
