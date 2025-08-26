61efbf3c5931d5ff799d7ab679b53afa95a4f8da
thepranavmehtaa <thepranavmehtaa@gmail.com>
2025-08-26 15:13:02 +0530

    Add data pipeline artifacts for 1 entities: UsagePlatform_fact_usage

--- curated/dml/UsagePlatform_fact_usage/sql/UsagePlatform_fact_usage_dml.sql
+++ curated/dml/UsagePlatform_fact_usage/sql/UsagePlatform_fact_usage_dml.sql
@@ -1,12 +1,38 @@
 -- DML Operations for UsagePlatform_fact_usage
--- Generated on: 2025-08-26T09:01:40.620Z
+-- Generated on: 2025-08-26T09:43:00.556Z
 
 -- Insert new records into curated table
 INSERT INTO curated.UsagePlatform_fact_usage (
-  
+  usage_id,
+  sub_id,
+  usage_date,
+  type,
+  volume,
+  minutes,
+  sms,
+  region,
+  roaming,
+  updated_at,
+  version,
+  is_deleted,
+  _ingest_timestamp,
+  _source_system,
+  _record_status,
+  _update_timestamp,
+  _batch_id,
+  _created_by,
+  _updated_by
 )
 SELECT 
-
+  usage_id as usage_id,
+  sub_id as sub_id,
+  usage_date as usage_date,
+  type as type,
+  volume as volume,
+  minutes as minutes,
+  sms as sms,
+  region as region,
+  roaming as roaming
 FROM raw.UsagePlatform_fact_usage;
 
 -- Update existing records
