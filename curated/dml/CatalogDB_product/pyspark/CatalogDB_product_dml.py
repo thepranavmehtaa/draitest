7cac4d50bc62af19dde6281501d5d070ffb7c8f8
thepranavmehtaa <thepranavmehtaa@gmail.com>
2025-08-26 14:41:48 +0530

    Add data pipeline artifacts for 1 entities: CatalogDB_product

--- curated/dml/CatalogDB_product/pyspark/CatalogDB_product_dml.py
+++ curated/dml/CatalogDB_product/pyspark/CatalogDB_product_dml.py
@@ -1,5 +1,5 @@
 # DML Operations for CatalogDB_product
-# Generated on: 2025-08-26T09:02:01.299Z
+# Generated on: 2025-08-26T09:11:15.879Z
 
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
@@ -22,7 +22,13 @@ new_records = raw_df.join(curated_df, "id", "left_anti")
 if new_records.count() > 0:
     # Transform new records according to mappings
     transformed_new = new_records.select(
-
+        col("prod_id").alias("prod_id"),
+        col("svc_id").alias("svc_id"),
+        col("prod_name").alias("prod_name"),
+        col("prod_type").alias("prod_type"),
+        col("bundle").alias("bundle"),
+        col("plan").alias("plan"),
+        col("discount").alias("discount")
     )
     
     # Add control columns
